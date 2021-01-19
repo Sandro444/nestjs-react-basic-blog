0e132f87711a9f0327afba2ef31748f18dbc2996
Sandro444 <tsereteli.sandro0401@gmail.com>
2021-01-19 20:16:00 +0400

    delete cache

--- frontend/src/gql-queries/queries/getCurrentUser.js
+++ frontend/src/gql-queries/queries/getCurrentUser.js
@@ -1,9 +1,5 @@
 import { gql } from "@apollo/client";
-<<<<<<< HEAD
-const GetCurrentUserQuery = gql`
-=======
 const getCurrentUserQuery = gql`
->>>>>>> 881a2d070dfea417345430e57da9c5102993217b
   query{
     getCurrentUser{
         username
@@ -12,8 +8,4 @@ const getCurrentUserQuery = gql`
   }
 `;
 
-<<<<<<< HEAD
-export default GetCurrentUserQuery;
-=======
 export default getCurrentUserQuery;
->>>>>>> 881a2d070dfea417345430e57da9c5102993217b
