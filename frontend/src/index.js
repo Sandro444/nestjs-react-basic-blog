316c9ab600602351db18824c406f92fc50eb9dfb
Sandro444 <tsereteli.sandro0401@gmail.com>
2021-01-23 16:00:06 +0400

    logout if unauthorized

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import {ContextLayer} from "./ContextLayer";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider,HttpLink, ApolloLink, concat } from "@apollo/client";
+import { onError } from '@apollo/client/link/error';
+import { useAuth } from "./hooks/common/auth/useAuth";
+
+const useLogoutWrapper = () => {
+  const {logOut} = useAuth()
+  logOut()
+}
 
 const httpLink = new HttpLink({ uri: 'http://localhost:3001/graphql' });
 
+const logoutLink = onError(({ networkError }) => {
+  if (networkError.statusCode === 401) useLogoutWrapper();
+})
 const authMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers = {} }) => ({
     headers: {
@@ -20,7 +29,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 })
 
 const client = new ApolloClient({
-  link: concat(authMiddleware, httpLink),
+  link: concat(authMiddleware, httpLink, logoutLink),
   cache: new InMemoryCache(),
 });
 
