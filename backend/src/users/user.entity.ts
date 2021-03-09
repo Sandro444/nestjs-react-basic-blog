eb935027e9e09d915a8682e5948152a086cb65a8
Sandro444 <tsereteli.sandro0401@gmail.com>
2021-03-09 10:48:49 +0400

    add current timestamp

--- backend/src/users/user.entity.ts
+++ backend/src/users/user.entity.ts
@@ -24,7 +24,7 @@ export class User {
   @Field((type) => Date)
   @Column({
     type: 'timestamp',
-    default: 'current_timestamp',
+    default: 'CURRENT_TIMESTAMP',
     name: 'created_at',
   })
   createdAt: Date;
